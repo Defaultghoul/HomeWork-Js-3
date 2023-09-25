4fcd3ff25c4772c4cd3414013e80548b317382bb
defaultghoul <dang6r.senpai@gmail.com>
2023-09-25 19:39:00 +0300

    wow

---
+++ script.js
@@ -0,0 +1,58 @@
+const btn = document.querySelector('.btn');
+
+btn.addEventListener('click', () => {
+	alert('HelloWorld');
+});
+
+// 2
+const inputValue = document.querySelector('#guessField');
+const pText = document.querySelector('#guessResult');
+
+const checkGuess = function () {
+	let rndNumber = Math.floor(Math.random() * (100 - 1) + 1);
+
+	if (rndNumber === +inputValue.value) {
+		pText.innerHTML = 'Ви вгадали!';
+	} else {
+		pText.innerHTML = 'Ви не вгадали!';
+	}
+};
+
+// task 3
+let counter = 0;
+
+document.addEventListener('click', () => {
+	counter++;
+	console.log(`Hа сторiнку натиснили ${counter}`);
+});
+//
+
+// 4
+const applyCallbackToArray = (arr, callback) => {
+	const result = [];
+	for (let i = 0; i < arr.length; i++) {
+		result.push(callback(arr[i]));
+	}
+	return result;
+};
+
+const square = num => {
+	return num * num;
+};
+
+const myArray = [1, 2, 3, 4, 5];
+
+console.log(applyCallbackToArray(myArray, square));
+
+// 5
+
+const calculateDiscountPrice = (price, discount, callback) => {
+	const newDiscount = showDiscountPrice(price, discount);
+	alert(`Ціна: ${price}, Знижка: ${discount}, Ціна з знижкою: ${newDiscount}`);
+};
+
+const showDiscountPrice = (price, discount) => {
+	return price - (price / 100) * discount;
+};
+
+calculateDiscountPrice(100, 10, showDiscountPrice);
